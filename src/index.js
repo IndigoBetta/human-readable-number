ab46b0aa5cb8096bf56118b2769f46c2db57d4ae
Helga <none@none.com>
2025-08-30 18:03:10 +0700

    feat: implimented function human-redable-number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function toReadable (number) {
-  
+
+  const ones = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    if (number < 20) {
+        return ones[number];
+    }
+    if (number < 100) {
+        const tenPart = Math.floor(number / 10);
+        const onePart = number % 10;
+        return onePart === 0 ? tens[tenPart] : tens[tenPart] + ' ' + ones[onePart];
+    }
+    const hundredPart = Math.floor(number / 100);
+    const rest = number % 100;
+    if (rest === 0) {
+        return ones[hundredPart] + ' hundred';
+    }
+    return ones[hundredPart] + ' hundred ' + toReadable(rest);
 }
