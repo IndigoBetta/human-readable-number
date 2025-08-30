c6a3845334b3a913f7f1d55172e2ad82bf896dd1
Helga <none@none.com>
2025-08-30 23:15:51 +0700

    fix: fix index.js

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,26 @@
+
+js
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-]);
-export default [
   {
-    files: ['**/*.js'],
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
     languageOptions: {
       ecmaVersion: 2021,
-      sourceType: 'module',
-    },
-    env: {
-      node: true,       
-      mocha: true,      
-      jest: true,      
-      browser: false,
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+        ...globals.mocha,
+        ...globals.jest,
+       
+      },
     },
     rules: {
-      
+     
     },
   },
-];
+]);
